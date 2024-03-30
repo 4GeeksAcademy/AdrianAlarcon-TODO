d462d13b048a265cc74a7242f27d4060436a56da
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-03-30 18:10:44 +0000

    a mitad

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
 };
+
+let listaTareas = document.querySelector("ul");
+
+let tareas = [];
+
+document.getElementById("agregarTarea").addEventListener("click", e => {
+  let nuevaTarea = document.getElementById("introducirTarea").value;
+  tareas = [];
+  tareas.push(nuevaTarea);
+  document.getElementById("introducirTarea").value = "";
+  tareas.map(tarea => {
+    listaTareas.innerHTML += `<li>${tarea}</li>`;
+  });
+  document.get;
+  let tareaIndependiente = document.querySelectorAll("li");
+  tareaIndependiente.forEach(LI =>
+    LI.addEventListener("click", e => {
+      LI.parentNode.removeChild(LI);
+    })
+  );
+});
